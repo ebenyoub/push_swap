08c66d9add3e06491ed502bec11082454ee47617
Elyas Benyoub <ebenyoub@me.com>
2021-09-18 22:10:53 +0200

    part 1 : done.

--- src/push_main.c
+++ src/push_main.c
@@ -6,7 +6,7 @@
 /*   By: ebenyoub <ebenyoub@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 00:04:23 by ebenyoub          #+#    #+#             */
-/*   Updated: 2021/09/15 01:23:07 by ebenyoub         ###   ########lyon.fr   */
+/*   Updated: 2021/09/15 20:57:57 by ebenyoub         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,5 @@ int     main(int argc, char **argv)
 	param_init(&argc, argv, &var);
 	var->lst_a = make_list(argc, &var->tab);
 	var->lst_b = new_list();
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-
-	psh_pb(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-
-	psh_pb(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-
-	rot_ra(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-	
-	rev_rra(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-
-	swp_sb(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-	
-	rot_ra(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-	
-	swp_ss(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
-	ft_putstr("\n");
-	
-	rev_rrr(var);
-	print_lst(var->lst_a->begin);
-	print_lst(var->lst_b->begin);
 	return (0);
 }
